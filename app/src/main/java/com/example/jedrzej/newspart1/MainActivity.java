ebb0717aaaf6f5d3a294746952268f5cd3d7e1c0
Jedrzej <idzikowskijedrzej@gmail.com>
2018-07-09 08:48:49 +0200

    Added settings activity, menu item in toolbar

--- app/src/main/java/com/example/jedrzej/newspart1/MainActivity.java
+++ app/src/main/java/com/example/jedrzej/newspart1/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -86,6 +88,25 @@ public class MainActivity
 
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, Settings.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/example/jedrzej/newspart1/Settings.java
@@ -0,0 +1,15 @@
+package com.example.jedrzej.newspart1;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+}
